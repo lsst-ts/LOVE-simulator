e8affba110917c0ee79f074600de42a9a396687f
sfehlandt <sebafehlandt@gmail.com>
2019-09-09 18:51:27 -0300

    Fix APIdoc

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -14,8 +14,7 @@ documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-# sys.path.insert(0, os.path.abspath('../../simulator'))
-# sys.path.insert(0, os.path.abspath('../../csc_sim'))
+sys.path.insert(0, os.path.abspath('../../simulator'))
 
 
 # -- Project information -----------------------------------------------------
