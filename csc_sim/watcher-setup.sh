74505df378dfd5264a336efc3d7ef20e4428d5d9
José  Galaz <jdgalazm@gmail.com>
2020-07-10 16:04:51 -0400

    update watcher-sim to b:101

--- csc_sim/watcher-setup.sh
+++ csc_sim/watcher-setup.sh
@@ -2,21 +2,7 @@
 
 # Source this file when starting the container to set it up
 
-echo "#"
-echo "# Loading LSST Stack"
-. /opt/lsst/software/stack/loadLSST.bash
-setup lsst_distrib
-echo "#"
-echo "# Loading sal environment"
-. repos/ts_sal/setup.env
-echo "#"
-echo "# Setting up sal, salobj and scriptqueue"
-
-setup ts_xml -t current
-setup ts_sal -t current
-setup ts_salobj -t current
-setup ts_scriptqueue -t current
-setup ts_config_attcs -t current
+. /home/saluser/.setup_dev.sh
 
 if [[ $LSST_DDS_IP != *"."* ]]; then
   echo "Unset LSST_DDS_IP"
@@ -49,9 +35,7 @@ echo "rules:
   - name: ScriptQueue:1" > default.yaml
 
 #RUN WATCHER
-cd /home/saluser/repos
-git clone https://github.com/lsst-ts/ts_watcher.git
-cd ts_watcher
+cd /home/saluser/repos/ts_watcher
 setup -r .
 scons install declare
-python bin/run_watcher.py
+python /home/saluser/repos/ts_watcher/bin/run_watcher.py
