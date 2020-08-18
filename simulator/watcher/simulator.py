77f29f74aa9538fcdcf004a75f467751f110d29c
José  Galaz <jdgalazm@gmail.com>
2020-08-18 17:38:38 -0400

    watcher simulator only enables it at the beginning

--- simulator/watcher/simulator.py
+++ simulator/watcher/simulator.py
@@ -21,23 +21,23 @@ async def main_csc(name, index, domain):
     except Exception as e:
         print(e)
 
-    while True:
-        try:
-            await asyncio.sleep(60)
-            print('Disabling watcher')
-            await r.cmd_disable.start()
-        except Exception as e:
-            print(e)    
-        try:
-            print('Enabling watcher')
-            await r.cmd_enable.start()
-        except Exception as e:
-            print(e)
-        try:
-            print('Starting watcher')
-            await r.cmd_start.start()
-        except Exception as e:
-            print(e)
+    # while True:
+    #     try:
+    #         await asyncio.sleep(60)
+    #         print('Disabling watcher')
+    #         await r.cmd_disable.start()
+    #     except Exception as e:
+    #         print(e)    
+    #     try:
+    #         print('Enabling watcher')
+    #         await r.cmd_enable.start()
+    #     except Exception as e:
+    #         print(e)
+    #     try:
+    #         print('Starting watcher')
+    #         await r.cmd_start.start()
+    #     except Exception as e:
+    #         print(e)
 
 
 async def main(csc_list):
