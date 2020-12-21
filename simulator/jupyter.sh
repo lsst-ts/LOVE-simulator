80baa1e25af007452d82c4627a3ed54eaf91eb88
spereirag <sebastian.pereira@inria.cl>
2020-12-21 10:50:38 -0300

    Make idl files in jupyter container

--- simulator/jupyter.sh
+++ simulator/jupyter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 . /home/saluser/.setup_dev.sh
-
+/home/saluser/repos/ts_sal/bin/make_idl_files.py LOVE
+pip install jupyterlab
 jupyter lab --no-browser --port=1234 --ip=0.0.0.0 --allow-root --NotebookApp.token=$JUPYTER_PASS
