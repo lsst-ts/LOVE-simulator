31a8c0b83a79bb972a025215cf17a9d767aaa91c
spereirag <sebastian.pereira@inria.cl>
2020-12-22 12:01:58 -0300

    Make CSCs run in async mode

--- csc_sim/scriptqueue.py
+++ csc_sim/scriptqueue.py
@@ -7,7 +7,7 @@ externalpath = '/home/saluser/repos/ts_scriptqueue/tests/data/external'
 
 simulator_config_path = '/home/saluser/config/config.json'
 
-if __name__ == '__main__':
+async def main():
     with open(simulator_config_path) as f:
         simulator_config = json.loads(f.read())
 
@@ -21,5 +21,6 @@ if __name__ == '__main__':
                                               standardpath=standardpath,
                                               externalpath=externalpath)
                 awaitables.append(csc.done_task)
+        await asyncio.wait(awaitables)
 
-    asyncio.get_event_loop().run_until_complete(asyncio.wait(awaitables))
+asyncio.run(main())
