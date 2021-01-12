013cbe6465b626f1db4f2fcf06374a88289ba708
spereirag <sebastian.pereira@inria.cl>
2021-01-12 19:47:16 -0300

    Remove unused code

--- simulator/scriptqueue/simulator.py
+++ simulator/scriptqueue/simulator.py
@@ -11,31 +11,3 @@ async def main(index):
     timeout = 30
 
     await salobj.set_summary_state(remote=r, state=salobj.State.ENABLED, timeout=timeout)
-
-    # isStandard = True
-    # path = 'script1'
-    # config_path = '/usr/src/love/simulator/scriptqueue/config.yml'
-    # with open(config_path, "r") as f:
-    #     config = f.read()
-    # location = Location.LAST
-
-    # counter = 0
-    # while True:
-    #     counter += 1
-    #     try:
-    #         await r.cmd_add.set_start(
-    #             isStandard=isStandard,
-    #             path=path,
-    #             config=config,
-    #             location=location,
-    #             timeout=timeout
-    #         )
-    #     except Exception as e:
-    #         print(e)
-    #     await asyncio.sleep(1)
-    #     if counter > 15:
-    #         break
-
-if __name__ == '__main__':
-    print('--starting scriptqueue-sim loop---')
-    asyncio.get_event_loop().run_until_complete(main())
