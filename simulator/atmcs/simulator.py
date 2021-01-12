23a29c1fa2127d1a8fd7329d05a7798f3318adfc
spereirag <sebastian.pereira@inria.cl>
2021-01-12 19:46:30 -0300

    Refactor complex code

--- simulator/atmcs/simulator.py
+++ simulator/atmcs/simulator.py
@@ -1,6 +1,43 @@
 import asyncio
+import random
 from lsst.ts import salobj
 
+async def simulation_step(
+                r,
+                azimuth, 
+                azimuthMax, 
+                elevation, 
+                elevationMax, 
+                nasmyth1_rotator_angle, 
+                nasmyth1_rotator_angle_max, 
+                nasmyth2_rotator_angle, 
+                nasmyth2_rotator_angle_max, 
+                trackingLoops, 
+                loop_count, 
+                loop_count2, 
+            ):
+    if loop_count == 1:
+        t = await r.cmd_startTracking.start(timeout=10)
+    r.cmd_trackTarget.set(azimuth=azimuth+azimuthMax/trackingLoops*loop_count, azimuthVelocity=3,
+                            elevation=elevation+elevationMax/trackingLoops*loop_count, elevationVelocity=3,
+                            nasmyth1_rotator_angle=nasmyth1_rotator_angle+nasmyth1_rotator_angle_max/trackingLoops*loop_count, nasmyth1_rotator_angleVelocity=3,
+                            nasmyth2_rotator_angle=nasmyth2_rotator_angle+nasmyth2_rotator_angleMax/trackingLoops*loop_count, nasmyth2_rotator_angleVelocity=3,
+                            taiTime=t.private_sndStamp, trackId=1)
+    if loop_count < trackingLoops:
+        t = await r.cmd_trackTarget.start(timeout=10)
+        await asyncio.sleep(0.25)
+    if loop_count > trackingLoops:
+        loop_count=0
+        await r.cmd_stopTracking.start(timeout=20)
+        r.cmd_setInstrumentPort.set(port=random.choice([1,2,3]))
+        r.evt_m3InPosition.flush()
+        await r.cmd_setInstrumentPort.start(timeout=10)
+        while True:
+            data = await r.evt_m3InPosition.next(flush=False)
+            if data.inPosition:
+                break
+        return 0
+    return loop_count + 1
 
 async def main_csc(name, index, domain):
     """ Runs the ATMCS simulator
@@ -25,13 +62,13 @@ async def main_csc(name, index, domain):
     azimuthMax = 260
     elevation = 10
     elevationMax = 70
-    nasmyth1RotatorAngle = 0
-    nasmyth1RotatorAngleMax = 90
-    nasmyth2RotatorAngle = 0
-    nasmyth2RotatorAngleMax = 90
+    nasmyth1_rotator_angle = 0
+    nasmyth1_rotator_angle_max = 90
+    nasmyth2_rotator_angle = 0
+    nasmyth2_rotator_angle_max = 90
     trackingLoops = 120
-    loopCount = 0
-    loopCount2 = 0
+    loop_count = 0
+    loop_count2 = 0
 
     class Object(object):
         pass
@@ -40,28 +77,20 @@ async def main_csc(name, index, domain):
     t.private_sndStamp = 0
     while True:
         try:
-            loopCount += 1
-            if loopCount == 1:
-                t = await r.cmd_startTracking.start(timeout=10)
-            r.cmd_trackTarget.set(azimuth=azimuth+azimuthMax/trackingLoops*loopCount, azimuthVelocity=3,
-                                    elevation=elevation+elevationMax/trackingLoops*loopCount, elevationVelocity=3,
-                                    nasmyth1RotatorAngle=nasmyth1RotatorAngle+nasmyth1RotatorAngleMax/trackingLoops*loopCount, nasmyth1RotatorAngleVelocity=3,
-                                    nasmyth2RotatorAngle=nasmyth2RotatorAngle+nasmyth2RotatorAngleMax/trackingLoops*loopCount, nasmyth2RotatorAngleVelocity=3,
-                                    taiTime=t.private_sndStamp, trackId=1)
-            if loopCount < trackingLoops:
-                t = await r.cmd_trackTarget.start(timeout=10)
-                await asyncio.sleep(0.25)
-            if loopCount > trackingLoops:
-                loopCount2 = loopCount2 + 1
-                loopCount=0
-                await r.cmd_stopTracking.start(timeout=20)
-                r.cmd_setInstrumentPort.set(port=(loopCount2 % 3+1))
-                r.evt_m3InPosition.flush()
-                await r.cmd_setInstrumentPort.start(timeout=10)
-                while True:
-                    data = await r.evt_m3InPosition.next(flush=False)
-                    if data.inPosition:
-                        break
+            loop_count = await simulation_step(
+                azimuth, 
+                azimuthMax, 
+                elevation, 
+                elevationMax, 
+                nasmyth1_rotator_angle, 
+                nasmyth1_rotator_angle_max, 
+                nasmyth2_rotator_angle, 
+                nasmyth2_rotator_angle_max, 
+                trackingLoops, 
+                loop_count, 
+                loop_count2, 
+            )
+            
         except Exception as e:
             print(e)
             try:
@@ -76,7 +105,7 @@ async def main_csc(name, index, domain):
                 await r.cmd_enable.start()
             except Exception as e:
                 pass
-            loopCount=0
+            loop_count=0
             await asyncio.sleep(10)
 
 
@@ -95,7 +124,3 @@ async def main(csc_list, domain):
         if csc[1] is not None:
             index = int(csc[1])
         asyncio.get_event_loop().create_task(main_csc(name, index, domain))
-
-if __name__ == '__main__':
-    print('***** Running ATMCS command simulator as standalone *****')
-    asyncio.get_event_loop().run_forever(main())
