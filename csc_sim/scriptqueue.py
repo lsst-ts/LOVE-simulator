48479233798e22921b4b4eea93772377c2198024
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2024-07-12 16:59:38 -0400

    Scriptqueue adjustments

--- csc_sim/scriptqueue.py
+++ csc_sim/scriptqueue.py
@@ -20,27 +20,25 @@
 
 import asyncio
 import json
+
 from lsst.ts import scriptqueue
 
-standardpath = '/home/saluser/repos/ts_scriptqueue/tests/data/standard/'
-externalpath = '/home/saluser/repos/ts_scriptqueue/tests/data/external/'
+simulator_config_path = "/home/saluser/config/config.json"
 
-simulator_config_path = '/home/saluser/config/config.json'
 
 async def main():
     with open(simulator_config_path) as f:
         simulator_config = json.loads(f.read())
 
-    if 'ScriptQueue' in simulator_config.keys():
+    if "ScriptQueue" in simulator_config.keys():
         awaitables = []
-        for scriptqueue_config in simulator_config['ScriptQueue']:
-            if scriptqueue_config['source'] == 'command_sim':
-                salindex = scriptqueue_config['index']
-                print('ScriptQueue csc | Launching salindex = {}'.format(salindex))
-                csc = scriptqueue.ScriptQueue(index=salindex,
-                                              standardpath=standardpath,
-                                              externalpath=externalpath)
+        for scriptqueue_config in simulator_config["ScriptQueue"]:
+            if scriptqueue_config["source"] == "command_sim":
+                salindex = scriptqueue_config["index"]
+                print("ScriptQueue csc | Launching salindex = {}".format(salindex))
+                csc = scriptqueue.ScriptQueue(index=salindex)
                 awaitables.append(csc.done_task)
         await asyncio.wait(awaitables)
+
 
 asyncio.run(main())
